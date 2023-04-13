490b82f8842a1462b18d61355272d74e647ab292
Ryan Peden <ryan@rpeden.com>
2023-04-13 11:43:34 -0400

    Added support for 'unreleased' docs versions

--- utilities/sort-docs-versions.py
+++ utilities/sort-docs-versions.py
@@ -3,6 +3,7 @@ import json
 
 from packaging.version import parse as parseVersion
 
+
 def get_versions_json(file_path):
     """Accepts a file path for versions.json and returns a dict containing
     the versions JSON file.
@@ -17,23 +18,40 @@ def get_versions_json(file_path):
         return json.load(f)
 
 
-def sort_versions_json(versions_json):
+def sort_versions_json(versions):
     """Accepts a list containing versions json dicts and sorts the
     versions in descending order.
 
     Args:
-        versions_json (list): A list of dicts containing version information
+        versions (list): A list of dicts containing version information
     Returns:
         dict: A dict containing the versions JSON file with sorted versions
     """
 
     # remove the "latest" version from the list if it exists
-    versions_json = [version for version in versions_json if version["version"] != "latest"]
+    versions = [
+        version for version in versions if version["version"] != "latest"
+    ]
+
+    invalid_versions_count = 0
+
+    def sort_key(version):
+        nonlocal invalid_versions_count
+        """ Uses the `parseVersion` function from the `packaging` module to 
+        parse a version string into a Version object if possible, 
+        otherwise returns a dummy Version that ensures the version will be added
+        to the end of the list
+        """
+        try:
+            return parseVersion(version["version"])
+        except ValueError:
+            invalid_versions_count += 1
+            return parseVersion("0.0.0")
 
     # use sorted() to sort the versions in descending order
     sorted_versions = sorted(
-        versions_json,
-        key=lambda version: parseVersion(version["version"]),
+        versions,
+        key=sort_key,
         reverse=True,
     )
 
@@ -43,6 +61,23 @@ def sort_versions_json(versions_json):
 
     # then, add the "latest" alias to the first version
     sorted_versions[0]["aliases"].append("latest")
+
+    # if any invalid versions were found, sort them in alphabetical order
+    # and update the list of versions since they are already added to the 
+    # end of the list.
+    if invalid_versions_count > 0:
+        # get the last n elements from the list where n is the number of
+        # invalid versions
+        invalid_versions = sorted_versions[-invalid_versions_count:]
+        
+        # remove the invalid versions from the list
+        sorted_versions = sorted_versions[:-invalid_versions_count]
+
+        # sort the invalid versions in alphabetical order
+        invalid_versions = sorted(invalid_versions, key=sort_key)
+
+        # re-add the invalid versions to the list
+        sorted_versions.extend(invalid_versions)
 
     return sorted_versions
 
@@ -69,5 +104,5 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     versions_json = get_versions_json(args.versions_file_path)
-    sorted_versions = sort_versions_json(versions_json)
-    save_versions_json(sorted_versions, args.versions_file_path)
+    updated_versions = sort_versions_json(versions_json)
+    save_versions_json(updated_versions, args.versions_file_path)
